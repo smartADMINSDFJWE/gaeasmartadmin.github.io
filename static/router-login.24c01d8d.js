ff6e743d6624395e9be31ffc7983cf3634a73fe1
cdcdx <cdcdx888@gmail.com>
2025-10-30 01:53:41 +0000

    update 2025-10-30 01:53:41

---
+++ static/router-login.24c01d8d.js
@@ -0,0 +1,1 @@
+import{b_ as o,Z as e}from"./entry.b4f5a8ba.js";import{u as t}from"./user.0c34512b.js";const a=o((r,s)=>{if(t().getToken){if(r.path==="/")return e("/home")}else return e("/login")});export{a as default};
